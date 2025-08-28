5f464a9a8837b14af53ad67d3c62410cebfca340
Cristian Nass <cristian.tirdad@gmail.com>
2025-08-28 22:34:49 +0200

    Add navbar

---
+++ components/view/Navbar.tsx
@@ -0,0 +1,44 @@
+const Navbar = () => {
+  const menuItems = [
+    {
+      id: 1,
+      label: "Snab Mat",
+    },
+    {
+      id: 2,
+      label: "Shopping",
+    },
+    {
+      id: 3,
+      label: "About Us",
+    },
+    {
+      id: 4,
+      label: "Contact Us",
+    },
+  ];
+
+  return (
+    <nav className='main-navbar'>
+      <ul
+        style={{
+          display: "flex",
+          listStyle: "none",
+          margin: 0,
+          padding: 0,
+          gap: "30px",
+          //   justifyContent: 'center',
+          width: "100%",
+        }}
+      >
+        {menuItems.map((item) => (
+          <li key={item.label} className='navbar-button'>
+            {item.label}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
